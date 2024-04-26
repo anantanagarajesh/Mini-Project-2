891fd7dc33248f3b95326bb7da38ac284a1e614f
Harini <hariniiii.48@gmail.com>
2024-04-26 06:14:34 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,127 +1,157 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
 const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
-	return res.render('index.ejs');
+    res.render('index.ejs');
 });
 
-
 router.post('/', (req, res, next) => {
-	let personInfo = req.body;
+    let personInfo = req.body;
 
-	if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
-		res.send();
-	} else {
-		if (personInfo.password == personInfo.passwordConf) {
+    // Log username and email
+    console.log('Signup - Username:', personInfo.username);
+    console.log('Signup - Email:', personInfo.email);
 
-			User.findOne({ email: personInfo.email }, (err, data) => {
-				if (!data) {
-					let c;
-					User.findOne({}, (err, data) => {
+    // Handle audio file if present
+    if (personInfo.recordedAudio) {
+        const audioBuffer = Buffer.from(personInfo.recordedAudio, 'base64');
+        const uploadsDir = path.join(__dirname, 'uploads');
 
-						if (data) {
-							c = data.unique_id + 1;
-						} else {
-							c = 1;
-						}
+        // Ensure the uploads directory exists
+        if (!fs.existsSync(uploadsDir)){
+            fs.mkdirSync(uploadsDir);
+        }
 
-						let newPerson = new User({
-							unique_id: c,
-							email: personInfo.email,
-							username: personInfo.username,
-							password: personInfo.password,
-							passwordConf: personInfo.passwordConf
-						});
+        const audioPath = path.join(uploadsDir, `${personInfo.username}-${Date.now()}.wav`);
 
-						newPerson.save((err, Person) => {
-							if (err)
-								console.log(err);
-							else
-								console.log('Success');
-						});
+        fs.writeFile(audioPath, audioBuffer, (err) => {
+            if (err) {
+                console.error('Error saving audio:', err);
+                return res.status(500).send({ "Success": "Failed to save audio" });
+            }
+            console.log('Recorded audio saved successfully:', audioPath);
+        });
+    }
 
-					}).sort({ _id: -1 }).limit(1);
-					res.send({ "Success": "You are regestered,You can login now." });
-				} else {
-					res.send({ "Success": "Email is already used." });
-				}
+    // Proceed with user registration
+    if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
+        res.send();
+    } else {
+        if (personInfo.password === personInfo.passwordConf) {
+            User.findOne({ email: personInfo.email }, (err, data) => {
+                if (!data) {
+                    User.findOne({}, (err, data) => {
+                        let c = 1;
+                        if (data) {
+                            c = data.unique_id + 1;
+                        }
 
-			});
-		} else {
-			res.send({ "Success": "password is not matched" });
-		}
-	}
+                        let newPerson = new User({
+                            unique_id: c,
+                            email: personInfo.email,
+                            username: personInfo.username,
+                            password: personInfo.password,
+                            passwordConf: personInfo.passwordConf
+                        });
+
+                        newPerson.save((err, Person) => {
+                            if (err)
+                                console.error(err);
+                            else
+                                console.log('Success');
+                        });
+                    }).sort({ _id: -1 }).limit(1);
+                    res.send({ "Success": "You are registered, You can login now." });
+                } else {
+                    res.send({ "Success": "Email is already used." });
+                }
+            });
+        } else {
+            res.send({ "Success": "Password is not matched" });
+        }
+    }
 });
 
 router.get('/login', (req, res, next) => {
-	return res.render('login.ejs');
+    res.render('login.ejs');
 });
 
 router.post('/login', (req, res, next) => {
-	User.findOne({ email: req.body.email }, (err, data) => {
-		if (data) {
+    let loginInfo = req.body;
+    
+    console.log('Login - Username:', loginInfo.email);
+    console.log('Login - Password:', loginInfo.password);
 
-			if (data.password == req.body.password) {
-				req.session.userId = data.unique_id;
-				res.send({ "Success": "Success!" });
-			} else {
-				res.send({ "Success": "Wrong password!" });
-			}
-		} else {
-			res.send({ "Success": "This Email Is not regestered!" });
-		}
-	});
+    User.findOne({ email: loginInfo.email }, (err, data) => {
+        if (data) {
+            if (data.password === loginInfo.password) {
+                req.session.userId = data.unique_id;
+                res.send({ "Success": "Success!" });
+            } else {
+                res.send({ "Success": "Wrong password!" });
+            }
+        } else {
+            res.send({ "Success": "This Email Is not registered!" });
+        }
+    });
 });
 
 router.get('/profile', (req, res, next) => {
-	User.findOne({ unique_id: req.session.userId }, (err, data) => {
-		if (!data) {
-			res.redirect('/');
-		} else {
-			return res.render('data.ejs', { "name": data.username, "email": data.email });
-		}
-	});
+    User.findOne({ unique_id: req.session.userId }, (err, data) => {
+        if (!data) {
+            res.redirect('/');
+        } else {
+            res.render('data.ejs', { "name": data.username, "email": data.email });
+        }
+    });
 });
 
 router.get('/logout', (req, res, next) => {
-	if (req.session) {
-		// delete session object
-		req.session.destroy((err) => {
-			if (err) {
-				return next(err);
-			} else {
-				return res.redirect('/');
-			}
-		});
-	}
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) {
+                return next(err);
+            } else {
+                return res.redirect('/');
+            }
+        });
+    }
 });
 
 router.get('/forgetpass', (req, res, next) => {
-	res.render("forget.ejs");
+    res.render("forget.ejs");
 });
 
 router.post('/forgetpass', (req, res, next) => {
-	User.findOne({ email: req.body.email }, (err, data) => {
-		if (!data) {
-			res.send({ "Success": "This Email Is not regestered!" });
-		} else {
-			if (req.body.password == req.body.passwordConf) {
-				data.password = req.body.password;
-				data.passwordConf = req.body.passwordConf;
+    let forgetInfo = req.body;
+    
+    console.log('Forget Password - Username:', forgetInfo.email);
+    console.log('Forget Password - New Password:', forgetInfo.password);
+    console.log('Forget Password - Confirm New Password:', forgetInfo.passwordConf);
 
-				data.save((err, Person) => {
-					if (err)
-						console.log(err);
-					else
-						console.log('Success');
-					res.send({ "Success": "Password changed!" });
-				});
-			} else {
-				res.send({ "Success": "Password does not matched! Both Password should be same." });
-			}
-		}
-	});
+    User.findOne({ email: forgetInfo.email }, (err, data) => {
+        if (!data) {
+            res.send({ "Success": "This Email Is not registered!" });
+        } else {
+            if (forgetInfo.password === forgetInfo.passwordConf) {
+                data.password = forgetInfo.password;
+                data.passwordConf = forgetInfo.passwordConf;
+
+                data.save((err, Person) => {
+                    if (err)
+                        console.error(err);
+                    else
+                        console.log('Success');
+                    res.send({ "Success": "Password changed!" });
+                });
+            } else {
+                res.send({ "Success": "Password does not match! Both Password should be same." });
+            }
+        }
+    });
 
 });
 
